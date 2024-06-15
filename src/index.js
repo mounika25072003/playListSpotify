8a9df9674fa89d5c7bc9aa1d58acf67a7f9e418e
mounika25072003 <jarupatimounika@gmail.com>
2024-06-15 07:21:24 +0000

    music

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App.jsx'
-import {StateProvider} from './utils/StateProvider'
-import reducer, {initialState} from './utils/Reducer'
+import {BrowserRouter} from 'react-router-dom'
+
+import App from './App'
+// import Player from './components/Player'
+
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
+    <BrowserRouter>
       <App />
-    </StateProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 )
